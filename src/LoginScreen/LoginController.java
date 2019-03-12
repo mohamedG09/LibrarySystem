fcf76cbe5281f6c0e4751c9eed7bf1cbc05acbd8
Mohamed <mohamed.abdelhady9999@gmail.com>
2019-03-13 00:18:19 +0400

    First Commit

---
+++ src/LoginScreen/LoginController.java
@@ -0,0 +1,4 @@
+package LoginScreen;
+
+public class LoginController {
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module LibrarySystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.swing;
+    requires com.jfoenix;
+    opens LoginScreen;
+}
